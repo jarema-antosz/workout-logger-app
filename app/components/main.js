1447339f0713a5224d28f4a5b4b5fa86ad9a714d
jarema-antosz <jarema.antosz@wp.pl>
2017-04-01 16:59:03 +0200

    json-server and first ajax call

--- app/components/main.js
+++ app/components/main.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Trainings from './trainings';
 
 var trainings = [
-                    {id: '1',
-                      date: '2017-01-01',
-                      exercises: [ {name: "pushups", reps: "30"}, { name: "pullups", reps: "10"}]
-                    },
-                    {id: '2',
-                      date: '2017-01-02',
-                      exercises: [ {name: "pushups", reps: "50"}, { name: "situps", reps: "30"}]
-                    }
+                    // {id: '1',
+                    //   date: '2017-01-01',
+                    //   exercises: [ {name: "pushups", reps: "30"}, { name: "pullups", reps: "10"}]
+                    // },
+                    // {id: '2',
+                    //   date: '2017-01-02',
+                    //   exercises: [ {name: "pushups", reps: "50"}, { name: "situps", reps: "30"}]
+                    // }
                     ];
 
 class MainComponent extends React.Component {
@@ -19,22 +19,32 @@ class MainComponent extends React.Component {
     this.deleteTraining = this.deleteTraining.bind(this);
   }
   render() {
+    if(this.state.trainings.length != 0)
     return (
       <div>Main Component Here
-        <div><Trainings trainingList= {this.state.trainings} onDelete={this.deleteTraining}/></div>
+          <div><Trainings trainingList= {this.state.trainings} onDelete={this.deleteTraining}/></div>
+      </div>
+    );
+    else
+    return (
+      <div>Main Component Here
+          <div>No data</div>
       </div>
     );
   }
 
   componentDidMount() {
-    console.log("MainComponent mounted");
+    console.log("MainComponent mounted. Make ajax call");
+    fetch("http://localhost:3000/trainings").then((resp) => resp.json())
+    .then((data) => {trainings= data; this.setState({trainings : trainings})})
+    .catch(function(error) {
+      console.log(error);
+    });
   }
 
   deleteTraining(training) {
-    console.log(training);
     trainings = trainings.filter( element => element.id !== training );
     this.setState({trainings : trainings});
-    console.log(trainings);
   }
 
 }
